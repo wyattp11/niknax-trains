0106cc6e47ff5caca52917fe86af53f39f332af6
Wyatt Patry <wyattpatry@gmail.com>
2026-06-17 22:01:07 +0000

    Wire up Vercel Analytics and Speed Insights

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject as injectAnalytics } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import router from './router/index.js'
 import App from './App.vue'
 import './assets/main.css'
+
+injectAnalytics()
+injectSpeedInsights()
 
 const app = createApp(App)
 app.use(createPinia())
